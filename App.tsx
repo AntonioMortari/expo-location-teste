545c623324b411e3ab10f21452912026863fd4af
Antonio Mortari <antoniomortari09@gmail.com>
2024-06-01 15:15:51 -0300

    fist commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
-export default function App() {
+import { LocationObject, getCurrentPositionAsync, requestForegroundPermissionsAsync } from 'expo-location';
+import { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+
+const App = () => {
+  const [coords, setCoords] = useState<LocationObject>({} as LocationObject);
+
+  const getUserLocation = async () => {
+    const { granted } = await requestForegroundPermissionsAsync();
+
+    if (granted) {
+      const currentPosition = await getCurrentPositionAsync();
+
+      setCoords(currentPosition);
+
+      const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${currentPosition.coords.latitude}&lon=${currentPosition.coords.longitude}`);
+      const data = await response.json();
+      const formattedAddress = data;
+      console.log(formattedAddress);
+    }
+  }
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Olá</Text>
+
+      {coords.coords && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: Number(coords.coords.latitude),
+            longitude: Number(coords.coords.longitude),
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: Number(coords.coords.latitude),
+              longitude: Number(coords.coords.longitude)
+            }}
+          />
+        </MapView>
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  map: {
+    width: '100%',
+    height: 400
+  }
+})
+
+export default App;
